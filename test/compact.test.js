39f6290f3ff65b4045757d704a79349db95007cc
Emilia Varpio <emilia.varpio@hotmail.fi>
2023-12-05 14:01:49 +0200

    compact arrays changed

--- test/compact.test.js
+++ test/compact.test.js
@@ -3,21 +3,21 @@ import memoize from '../src/compact.js';
 
 describe('compact.js', () => {
   it('should remove invalid values', () => {
-    const array = [false, null, 0, "", undefined, NaN];
+    var array = [false, null, 0, "", undefined, NaN];
     const result = compact(array);
 
     expect(result).to.equal([]);
   });
 
   it('should not remove valid values', () => {
-    const array = [1, 2, 3];
+    var array = [1, 2, 3];
     const result = compact(array);
 
     expect(result).to.equal([1, 2, 3]);
   });
 
   it('should work with a mix of valid and invalid values', () => {
-    const array = ["one", null, "two", "three", undefined];
+    var array = ["one", null, "two", "three", undefined];
     const result = compact(array);
 
     expect(result).to.equal([1, 2, 3]);
